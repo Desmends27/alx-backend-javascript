4f7bc3c27626facbd84053622c6810aaf053db02
Desmends27 <desmondopokumends2004@gmail.com>
2024-06-27 02:21:16 +0000

    linter

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-    promise.then(() => {
-        return {
-            'status': 200,
-            'body': 'success'
-        }
-    }).catch(new Error()).then(() => {
-        console.log("Got a response from the API")
-    })
+  promise.then(() => ({
+    status: 200,
+    body: 'Success',
+  })).catch(new Error()).then(() => console.log('Got response from the API'));
 }
