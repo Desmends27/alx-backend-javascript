525b0e7298ca938e4ec9e8168244ffeacb9f8072
Desmends27 <desmondopokumends2004@gmail.com>
2024-06-28 16:17:30 +0000

    linter

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-	
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const [photo, user] = results;
