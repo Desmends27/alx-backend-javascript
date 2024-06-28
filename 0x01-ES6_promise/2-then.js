f2e04f3ddca484944b7c5db3bef25febdb62aae7
Desmends27 <desmondopokumends2004@gmail.com>
2024-06-28 16:00:08 +0000

    handleResponse

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return {
-      status: 200,
-      body: 'Success',
-    };
-  }).catch(() => {
-    console.log('Got a response from the API');
-    return new Error();
-  });
+  promise.then(() => ({
+    status: 200,
+    body: 'success',
+  })).catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
