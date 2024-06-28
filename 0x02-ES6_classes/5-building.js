1c826c8399bac66eed60536cf00f514cd031bfd3
Desmends27 <desmondopokumends2004@gmail.com>
2024-06-28 23:37:44 +0000

    linting issues

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building &&
-	    typeof this.evacuationWarningMessage !== 'function')
-	  {
-	     throw new Error('Class extending Building must override evacuationWarningMessage');
-	  }
+    if (this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
