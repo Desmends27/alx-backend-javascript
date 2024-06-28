4ce90640362499f12dca9a530adc4a762f3871f5
Desmends27 <desmondopokumends2004@gmail.com>
2024-06-28 16:23:27 +0000

    test

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,9 @@ import signUpUser from './4-user-promise'
 import uploadPhoto from './5-photo-reject'
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-	Promise.allSettled([signUpUser(firstName, lastName),
+	return Promise.allSettled([signUpUser(firstName, lastName),
 			uploadPhoto(fileName)])
-	.then((results => {
-		return results.map((result) => {
-			'status': result.status,
-			'value': result.value,
-		});
-	)});
+	.then((results)=> {
+		return results;
+	});
 }
