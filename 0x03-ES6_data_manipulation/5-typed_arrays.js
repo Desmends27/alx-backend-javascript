30596c6955aa4fce33103fd6c7482d4e5ea9a792
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-03 01:16:50 +0000

    test

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,13 +1,11 @@
-export default function createInt8TypedArray(length, position, value)
-{
-	const arr = new ArrayBuffer(length);
-	const view = new DataView(arr);
+export default function createInt8TypedArray(length, position, value) {
+  const arr = new ArrayBuffer(length);
+  const view = new DataView(arr);
 
-	try{
-		view.setInt8(position, value)
-	}
-	catch (error) {
-		throw new Error("Position outside range")
-	}
-	return arr;
+  try {
+    view.setInt8(position, value);
+  } catch (error) {
+    throw new RangeError('Position outside range');
+  }
+  return arr;
 }
