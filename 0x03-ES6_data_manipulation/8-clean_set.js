951a0a5c58bcd9c0ba6d920d44a823508bbf5680
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-03 02:27:33 +0000

    added check for set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 export default function cleanSet(set, startString) {
   const str = [];
+  if (!(set instanceof Set)) {
+    return '';
+  }
   if (startString !== '') {
     for (const item of set) {
       if (item.startsWith(startString)) {
